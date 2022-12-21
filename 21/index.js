b7ef650e45a7855f9cc1a2e60b622842480c89d7
tungmn <ngoctung.mai@vmgmedia.vn>
2022-12-21 21:09:00 +0700

    solve day 21

--- 21/index.js
+++ 21/index.js
@@ -14,17 +14,13 @@ data.split(/\r?\n/).forEach(el => {
     map[arr[0]] = arr[1]
 })
 
-
-
-console.log(cal('root'))
-
 function cal(key){
     let value = map[key]
     let number = Number(value)
+
     if(!isNaN(number)) {
         return number 
     }
-
     let values = value.split(' ')
     if(isNaN(Number(values[0]))){
         values[0] = cal(values[0])
@@ -33,5 +29,112 @@ function cal(key){
         values[2] = cal(values[2])
     }
     const expr = values.join(' ')
+    if(key === 'root'){
+        console.log(expr)
+    }
     return eval(expr)
 }
+
+function hasHumn(key){
+    let value = map[key]
+    let rs = false
+    if(key === 'humn') {
+        return true 
+    }
+    let number = Number(value)
+    if(!isNaN(number)) {
+        return false 
+    }
+    let values = value.split(' ')
+    let first = false
+    let last = false
+    if(isNaN(Number(values[0]))){
+        first = hasHumn(values[0])
+    }
+    if(isNaN(Number(values[2]))){
+        last = hasHumn(values[2])
+    }
+
+    return rs || first || last
+}
+
+function traceHumn(total, key){
+    let value = map[key]
+    let number = Number(value)
+    if(!isNaN(number)) {
+        return total
+    }
+    let values = value.split(' ')
+    let first = false
+    if(isNaN(Number(values[0]))){
+        first = hasHumn(values[0])
+    }
+
+    let remain = 0
+    let nxt = ''
+    if(first){
+        remain = cal(values[2])
+        nxt = values[0]
+    }else{
+        remain = cal(values[0])
+        nxt = values[2]
+    }
+
+    const ops = values[1]
+    switch(ops){
+        case '+': 
+            total -= remain
+        break;
+        case '-': 
+            if(first){
+                total += remain
+            }else{
+                total = remain - total
+            }
+        break;
+        case '*':
+            total /= remain
+        break;
+        case '/': 
+            if(first){
+                total *= remain
+            }else{
+                total = remain / total
+            }
+        break;
+    }
+    
+    if(key === 'humn'){
+        return total
+    }else{
+        return traceHumn(total, nxt)
+    }
+
+}
+
+let values = map['root'].split(' ')
+let first = false
+let last = false
+if(isNaN(Number(values[0]))){
+    first = hasHumn(values[0])
+}
+if(isNaN(Number(values[2]))){
+    last = hasHumn(values[2])
+}
+console.log(values[0], first)
+console.log(values[2], last)
+
+let total = 0
+let key = ''
+if(first){
+    key = values[0]
+    total = cal(values[2])
+}else{
+    key = values[2]
+    total = cal(values[0])
+}
+console.log(total,key)
+let humn = traceHumn(total,key)
+console.log(humn)
+map.humn = humn
+console.log('cal(): ', cal('root'))
